6ec67f7bf803e15f3b43afeb65369f04867c0d9d
c000 <chikusat@hotmail.com>
2019-04-03 16:17:49 +0900

    added tests for NilOrder

---
+++ compare/compare_test.go
@@ -0,0 +1,25 @@
+package compare
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNilOrder(t *testing.T) {
+	i := 1
+	var n *int
+	e := &i
+
+	assert.Equal(t, 0, NilFirst.compare(n, n))
+	assert.Equal(t, 0, NilLast.compare(n, n))
+
+	assert.Equal(t, 0, NilFirst.compare(e, e))
+	assert.Equal(t, 0, NilLast.compare(e, e))
+
+	assert.Equal(t, -1, NilFirst.compare(n, e))
+	assert.Equal(t, -1, NilLast.compare(e, n))
+
+	assert.Equal(t, 1, NilFirst.compare(e, n))
+	assert.Equal(t, 1, NilLast.compare(n, e))
+}
